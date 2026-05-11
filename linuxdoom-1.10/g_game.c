7aac95145575d7823d184370f9cf224674562920
Evan Rinehart <evanrinehart@gmail.com>
2026-05-11 15:09:43 -0500

    stock demos restored
    demos recorded with doom version 1.09 now play including
    
    shareware doom 1
    doom 2
    ultimate doom
    final doom (tnt, plutonia)

--- linuxdoom-1.10/g_game.c
+++ linuxdoom-1.10/g_game.c
@@ -1567,12 +1567,12 @@ void G_DeferedPlayDemo (char* name)
 void G_DoPlayDemo (void) 
 { 
     skill_t skill; 
-    int             i, episode, map; 
+    int             i, episode, map, ver;
 	 
     gameaction = ga_nothing; 
     demobuffer = demo_p = W_CacheLumpName (defdemoname, PU_STATIC); 
-    if ( *demo_p++ != VERSION)
-    {
+    ver = *demo_p++;
+    if (ver != 109 && ver != VERSION) {
       fprintf( stderr, "Demo is from a different game version!\n");
       gameaction = ga_nothing;
       return;
